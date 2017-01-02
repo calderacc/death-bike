387c81b1279e846a0343a00789f85a4a1573034a
Malte Hübner <maltehuebner@gmx.org>
2017-01-02 23:53:06 +0100

    Require curl class and jms serializer bundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Caldera\Bundle\DeathBikeBundle\CalderaDeathBikeBundle()
+            new Caldera\Bundle\DeathBikeBundle\CalderaDeathBikeBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
