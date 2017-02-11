efcd9033bb26354f28fc64757909244397ba2727
Malte Hübner <maltehuebner@gmx.org>
2017-02-11 11:48:25 +0100

    Remove more annotation routing.

--- src/AppBundle/Controller/StatisticController.php
+++ src/AppBundle/Controller/StatisticController.php
@@ -2,18 +2,15 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class StatisticController extends Controller
 {
-    /**
-     * @Route("/statistic/{year}", name="statistic")
-     */
     public function indexAction(Request $request, int $year = null): Response
     {
+
         if (!$year) {
             $dateTime = new \DateTime();
             $year = $dateTime->format('Y');
